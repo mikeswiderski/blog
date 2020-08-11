160b7b1c86302ae0e0e5ce870168a3c10f9aa139
Michal Swiderski <contact@michalswiderski.com>
2020-08-11 06:30:16 -0400

    Replace main page with something custom, not default django frontpage #6

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render (request, 'dashboard/home.html')
+    return render(request, 'dashboard/home.html')
