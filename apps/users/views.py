fbb391c60fa6051dc9e6d01eb09f33d48aef0074
Michal Swiderski <contact@michalswiderski.com>
2020-08-14 18:39:15 -0400

    Sign up page #8

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,1 +1,12 @@
-#from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
