eba9b6502e2bc474b597a8285130a6ca7661fe51
Michal Swiderski <contact@michalswiderski.com>
2020-08-27 19:33:51 -0400

    Logged in user can create new blog post #15

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    body = models.TextField() 
+
+    def __str__(self):
+        return self.title
--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='dashboard-home'),
+    path('posts/<int:post_id>/', views.post_detail_view, name='post-detail'),
+    path('posts/new/', views.post_create_view, name='post-create'),
 ]
