6ec8b3735743955f900e54a52d904ff1928bfbcc
Michal Swiderski <contact@michalswiderski.com>
2020-11-15 15:50:15 -0500

    Post's tags #19

--- apps/posts/forms.py
+++ apps/posts/forms.py
@@ -5,6 +5,8 @@ from apps.tags.models import Tag
 import re
 
 
+pattern = re.compile(r'^[a-zA-Z0-9- ]*$')
+
 class PostModelForm(ModelForm):
 
     tags = forms.CharField(max_length=255, required=False)
@@ -20,20 +22,19 @@ class PostModelForm(ModelForm):
     def clean_tags(self):
         tags = self.cleaned_data.get('tags', None)
         clean_tags = []
-        validated_tags = []
 
         tags = [tag.strip() for tag in tags.split(',') if tags]
 
         for tag in tags:
-            if re.match('^[a-zA-Z0-9- ]*$', tag):
-                validated_tags.append(tag)
+            if pattern.match(tag):
+                continue
             else:
                 raise forms.ValidationError('Letters, digits, space, dash only.')
 
-        if len(tags) == len(validated_tags):
-            for tag in validated_tags:
-                t, created = Tag.objects.get_or_create(label=tag)
-                t.save()
-                clean_tags.append(t)
+        
+        for tag in tags:
+            t, created = Tag.objects.get_or_create(label=tag)
+            t.save()
+            clean_tags.append(t)
     
         return clean_tags
